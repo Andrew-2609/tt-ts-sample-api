b87f72465d5951745400ab6adf24d9ad80760a7b
Andrew Silva <andrew.agoravai@gmail.com>
2024-08-25 18:57:55 -0300

    refactor: create barrel export for app

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,6 @@
 import { logger } from '@/modules/common/logger'
 import { APP_CONFIG, loadEnvironment } from '../modules/common/config/environment'
-import { createExpressApp } from './app/app'
+import { createExpressApp } from './app'
 ;(async (): Promise<void> => {
   await loadEnvironment()
 
